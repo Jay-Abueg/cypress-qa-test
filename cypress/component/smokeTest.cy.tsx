73bb0b39935abd55936097a70e2b45be2bb89c8b
Jay-Abueg <55839272+Jay-Abueg@users.noreply.github.com>
2022-09-12 06:49:06 +0800

    Some minor changes

--- cypress/component/smokeTest.cy.tsx
+++ cypress/component/smokeTest.cy.tsx
@@ -18,32 +18,36 @@ describe('Smoke Test', () => {
     cy.contains(/ImageQuix QA Automation Test/i).should('be.visible')
     cy.contains('Go').should('be.visible')
     cy.get('[class=subreddit-input]').should('be.visible')
+
   })
 
   it('Verify display content /all',() => {
 
     cy.contains('Go').click()
+    
   })
 
   it('Verifies all routes contains text with "posted to /r/"', () => {
 
     cy.get('[class=author]').should('include.text', 'posted to /r/')
+
   })
 
-  it('Verify that the search bar loads the posts for inputted subreddit', () => {
+  it('Verify that the search bar loads the posts for inputted subreddit or throws an error message if non existent', () => {
 
     cy.get('[class=subreddit-input]').type(subredditInput+'{enter}')
     cy.get('[class=post]').should('not.be.empty') || cy.should('include.text', 'Could not retrive results for')
+
   })
 
-  it('Verifies the count returned vs. post rendered', () => {
+  it('Verifies the count returned vs. post rendered', () => { //will fail if there is no subreddit post return
 
     cy.get('[class=subreddit-input]').type(subredditInput+'{enter}')
-   
     cy.get('[class=post]').then($elements => {
       let countOfElements = 0
       countOfElements = $elements.length;
       cy.log("count:"+countOfElements)
+
     });
 
   })
